918d504102d8942f620d05eb3d643cd3cac0ba87
Herandom <46648268+Herandom@users.noreply.github.com>
2024-12-02 22:03:00 +0100

    read files was tricky

--- 2024/day1/main.go
+++ 2024/day1/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hejsan")
+	var lefts, rights []int
+	filename := "test_data.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "   ")
+		left, _ := strconv.Atoi(line[0])
+		right, _ := strconv.Atoi(line[1])
+		lefts = append(lefts, left)
+		rights = append(rights, right)
+		fmt.Printf("%v, %v \n", lefts, rights)
+	}
 }

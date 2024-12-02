b2031778af2db4502d5148d7b4d8d452aa71d2fe
Herandom <46648268+Herandom@users.noreply.github.com>
2024-12-02 22:58:41 +0100

    test input works, fuck inplace operations sometimes #skillissue

--- 2024/day1/main.go
+++ 2024/day1/main.go
@@ -4,9 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+
 	"strconv"
 	"strings"
 )
+
+func abs(val int) int{
+	if val < 0{
+		return -val
+	}
+	return val
+}
 
 func main() {
 	var lefts, rights []int
@@ -23,5 +32,14 @@ func main() {
 		lefts = append(lefts, left)
 		rights = append(rights, right)
 		fmt.Printf("%v, %v \n", lefts, rights)
+
 	}
+	defer file.Close()
+	sort.Ints(lefts)
+	sort.Ints(rights)
+	diff := 0
+	for ix := 0; ix < len(lefts); ix++{
+		diff += abs(lefts[ix] - rights[ix])
+	}
+	fmt.Println(diff)
 }

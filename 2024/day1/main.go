e6911431b09b9fd3f6596b93954fb6d5ddb00e6d
Herandom <46648268+Herandom@users.noreply.github.com>
2024-12-02 23:45:00 +0100

    day 1 complete

--- 2024/day1/main.go
+++ 2024/day1/main.go
@@ -10,14 +10,14 @@ import (
 	"strings"
 )
 
-func abs(val int) int{
-	if val < 0{
+func abs(val int) int {
+	if val < 0 {
 		return -val
 	}
 	return val
 }
 
-func main() {
+func task1() {
 	var lefts, rights []int
 	//	filename := "test_data.txt"
 
@@ -33,15 +33,60 @@ func main() {
 		right, _ := strconv.Atoi(line[1])
 		lefts = append(lefts, left)
 		rights = append(rights, right)
-	//	fmt.Printf("%v, %v \n", lefts, rights)
+		//	fmt.Printf("%v, %v \n", lefts, rights)
 
 	}
 	defer file.Close()
 	sort.Ints(lefts)
 	sort.Ints(rights)
 	diff := 0
-	for ix := 0; ix < len(lefts); ix++{
+	for ix := 0; ix < len(lefts); ix++ {
 		diff += abs(lefts[ix] - rights[ix])
 	}
 	fmt.Println(diff)
+}
+
+func task2() {
+	var lefts, rights []int
+	//filename := "test_data.txt"
+
+	filename := "input.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "   ")
+		left, _ := strconv.Atoi(line[0])
+		right, _ := strconv.Atoi(line[1])
+		lefts = append(lefts, left)
+		rights = append(rights, right)
+		//	fmt.Printf("%v, %v \n", lefts, rights)
+
+	}
+	defer file.Close()
+	sort.Ints(lefts)
+	sort.Ints(rights)
+	my_map := make(map[int]int)
+	val_count := make(map[int]int)
+	for _, val := range lefts {
+		my_map[val] = 0
+		val_count[val] += 1
+	}
+	for _, val := range rights {
+		_, ok := my_map[val]
+		if ok {
+			my_map[val] += 1
+		}
+	}
+	sim := 0
+	for val, count := range my_map {
+		sim += val * count * val_count[val]
+	}
+	fmt.Println(sim)
+}
+func main() {
+	// (	task1()
+	task2()
 }
---
+++ 2024/diary.md
@@ -0,0 +1,11 @@
+# Advent of Code 2024
+## 2024-12-02
+Did day1 and 2 in Python at work as some self learning, went quite smoothly (besides misreading the task)
+
+At home, did day 1. File IO was the first hurdle but finally figured it out. The second hurdle was the fact that `sort.Ints` sorts values in place and doesn't have a return. That was painful and took way, way, way to long (like at least 30min+ if not more. Then it turns out that Go doesn't have an `abs(val int) int` function so I had to implement that one (not that it's difficult but still). Final hurdle was the fact that I went with maps (almost python dicts) and the syntax around that. if-ok solved the issue of inserting values from right by mistake, and then a second `value_count` map to make the multiplication factor correct...
+
+### Learnings
+* File IO
+* no int-abs func
+* sort functions are in-place
+* if-ok statements.
